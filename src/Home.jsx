a508b796c1d87e4f195a5a0ea98004e99fab74d3
Ibrahim Sadique <abubakasaddik1@gmail.com>
2022-10-28 08:15:28 +0000

    data from json server

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,26 +2,7 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: 'my new website',
-      body: 'lorem ipsum ...',
-      author: 'saddik',
-      id: '1',
-    },
-    {
-      title: 'the web effect',
-      body: 'lorem ipsum ...',
-      author: 'samira',
-      id: '2',
-    },
-    {
-      title: 'the dev tips',
-      body: 'lorem ipsum ...',
-      author: 'paris',
-      id: '3',
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState('mario');
 
@@ -31,12 +12,25 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log('use effect ran');
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
   }, []);
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='all blogs!' handleDelete={handleDelete} />
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title='all blogs!'
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
