51305c0f5c537e777a81baf17cbaa50e6d2a60f9
Ibrahim Sadique <abubakasaddik1@gmail.com>
2022-10-31 07:17:05 +0000

    handled fetch error

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,17 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data);
-          setLoading(false); // do not show loading message after data is fetched
-        });
-    }, 1000);
-  }, []);
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className='home'>
+      {error && <div>{error} </div>}
       {isLoading && <div>Loading...</div>}
 
       {blogs && <BlogList blogs={blogs} title='all blogs!' />}
